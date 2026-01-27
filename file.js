e69bbfad358886ecda32086cb7b0a6201b800d13
Hoarau-arnaud <hoarau.arnaud@outlook.fr>
2026-01-27 08:34:04 +0100

    Mon commit 2

--- file.js
+++ file.js
@@ -1,7 +1,28 @@
-console.log('Hello, world!');
+console.log("Hello, world!");
 
 const a = 12;
 let b = 45;
 
-b += a ;
+b += a;
 console.log("resultat de a + b = " + b);
+
+let aPerson = {
+  name: "Jean",
+  age: 35,
+  gender: "masculin",
+  interest: ["musique", "Jeux videos"],
+};
+
+// on peut également déclarer et initialiser un objet
+//  de la façon suivante :
+/*
+let aPerson = new Object();
+aPerson.name = "Jean";
+aPerson.age = 35;
+aPerson.gender = "masculin";
+aPerson.interest = ["musique", "badminton"];
+*/
+
+aPerson.sayHello = function() {console.log('Bonjour ! Je suis ' + this.name + '.'); };
+
+aPerson.sayHello();
